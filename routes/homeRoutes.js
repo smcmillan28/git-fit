cfddf220a6bd01ecf115f5423dfd891f2e4588d1
Sammy McMillan <74073409+smcmillan28@users.noreply.github.com>
2021-04-15 13:03:41 -0400

    user can now navigate between each html page

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -1,19 +1,9 @@
 // Import dependencies
 const router = require('express').Router();
-// const Workout = require('../models/Workout.js');
+const Workout = require('../models/Workout.js');
 
-// Define get routes for each html page
-router.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname + "../public/index.html"));
-});
 
-router.get("/exercise", (req, res) => {
-    res.sendFile(path.join(__dirname + "../public/exercise.html"));
-});
 
-router.get("/stats", (req, res) => {
-    res.sendFile(path.join(__dirname + "../public/stats.html"));
-});
 
 // Export server
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // Will need to establish connection and dependencies through this file
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require("path");
 
 const PORT = process.env.PORT || 3000;
 
@@ -10,6 +11,20 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
+
+// Defining routes to each html page
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname + "/public/index.html"));
+});
+
+app.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname + "/public/exercise.html"));
+});
+
+app.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname + "/public/stats.html"));
+});
+
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
     useNewUrlParser: true,
