df98d416c080a5514f960f43c1e9f57a9bec260c
Sammy McMillan <74073409+smcmillan28@users.noreply.github.com>
2021-04-15 13:40:31 -0400

    home page pulling some of the information, still need to figure out duration error and posting new exercises

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 const workoutRoutes = require('./workoutRoutes');
 
 // Define router paths
-router.use('/workout', workoutRoutes);
+router.use('/workouts', workoutRoutes);
 
 // Export paths
 module.exports = router;
--- routes/api/workoutRoutes.js
+++ routes/api/workoutRoutes.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const Workout = require("../../models/Workout.js");
 
-router.post("/api/workout", ({ body }, res) => {
+router.post("/", ({ body }, res) => {
     Workout.create(body)
         .then(dbWorkout => {
             res.json(dbWorkout);
@@ -11,7 +11,7 @@ router.post("/api/workout", ({ body }, res) => {
         });
 });
 
-router.post("/api/workout/bulk", ({ body }, res) => {
+router.post("/bulk", ({ body }, res) => {
     Workout.insertMany(body)
         .then(dbWorkout => {
             res.json(dbWorkout);
@@ -21,7 +21,7 @@ router.post("/api/workout/bulk", ({ body }, res) => {
         });
 });
 
-router.get("/api/workout", (req, res) => {
+router.get("/", (req, res) => {
     Workout.find({})
         .sort({ date: -1 })
         .then(dbWorkout => {
