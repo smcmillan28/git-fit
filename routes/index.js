f7a6af644283d267e3e3349be7f76df3016e140c
Sammy McMillan <74073409+smcmillan28@users.noreply.github.com>
2021-04-15 12:47:49 -0400

    apiRoutes exported, need to define homeRoutes

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
 
 // Define middleware
-router.use('/', homeRoutes);
+// router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
 
 // Exporting to use in server.js file
