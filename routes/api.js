bbd0115826e505689dc8d9c2faec0ebe53c1eda6
Sammy McMillan <74073409+smcmillan28@users.noreply.github.com>
2021-04-19 20:49:36 -0400

    starting put/patch routes

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
-const Workout = require("../models/Workout");
+const { Workout } = require("../models");
+// const Workout = require("../models/Workout");
 const db = require("../models");
 
 router.post("/api/workouts", ({ body }, res) => {
@@ -11,6 +12,27 @@ router.post("/api/workouts", ({ body }, res) => {
             res.status(400).json(err);
         });
 });
+
+// router.patch("/api/workouts/:id", async (req, res) => {
+//     try {
+//         const workout = await db.Workout.findOne({ _id: req.params.id })
+
+//         if (workoutType === "cardio") {
+//             workoutData.type = "cardio";
+//             workoutData.name = cardioNameInput.value.trim();
+//             workoutData.distance = Number(distanceInput.value.trim());
+//             workoutData.duration = Number(durationInput.value.trim());
+//         } else if (workoutType === "resistance") {
+//             workoutData.type = "resistance";
+//             workoutData.name = nameInput.value.trim();
+//             workoutData.weight = Number(weightInput.value.trim());
+//             workoutData.sets = Number(setsInput.value.trim());
+//             workoutData.reps = Number(repsInput.value.trim());
+//             workoutData.duration = Number(resistanceDurationInput.value.trim());
+//         }
+
+//     }
+// })
 
 // router.post("/api/workouts/bulk", ({ body }, res) => {
 //     Workout.insertMany(body)
